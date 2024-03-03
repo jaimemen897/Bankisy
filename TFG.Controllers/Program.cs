ba82e0e16f43602ccccf9b0cd9c21e83683015d2
jaimemen897 <jaimemen897@gmail.com>
2024-03-03 16:58:15 +0100

    Env read

--- TFG.Controllers/Program.cs
+++ TFG.Controllers/Program.cs
@@ -2,14 +2,21 @@ using Microsoft.EntityFrameworkCore;
 using TFG.Context.Context;
 using TFG.Services;
 
-var builder = WebApplication.CreateBuilder(args);
+DotNetEnv.Env.Load();
+var host = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+var port = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+var database = Environment.GetEnvironmentVariable("DATABASE_NAME");
+var user = Environment.GetEnvironmentVariable("DATABASE_USER");
+var password = Environment.GetEnvironmentVariable("DATABASE_PASSWORD");
+var connectionString = $"Host={host};Port={port};Database={database};Username={user};Password={password}";
 
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<UsersService>();
 builder.Services.AddScoped<BankAccountService>();
 builder.Services.AddDbContext<BankContext>(options =>
 {
-    options.UseNpgsql("Host=localhost;Database=database;Username=admin;Password=admin");
+    options.UseNpgsql(connectionString);
 });
 
 var app = builder.Build();
@@ -23,10 +30,6 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 app.UseRouting();
 
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
 app.MapControllerRoute(
     name: "Users",
     pattern: "users/{action=Index}/{id?}");
@@ -35,6 +38,5 @@ app.MapControllerRoute(
     pattern: "bankAccounts/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html");
-;
 
 app.Run();
