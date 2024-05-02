31d524c52108d35fcbd3d8f7e0cc4b0787e11b6a
jaimemen897 <jaimemen897@gmail.com>
2024-05-02 17:40:38 +0200

    Se añaden los dialog al navbar

--- TFG.Controllers/ClientApp/src/app/navbar/navbar.component.ts
+++ TFG.Controllers/ClientApp/src/app/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject, OnInit, ViewChild} from '@angular/core';
 import {MenubarModule} from "primeng/menubar";
 import {ButtonModule} from "primeng/button";
-import {DOCUMENT, NgIf} from "@angular/common";
+import {DatePipe, DOCUMENT, NgIf} from "@angular/common";
 import {DropdownModule} from "primeng/dropdown";
 import {ToastModule} from "primeng/toast";
 import {ConfirmPopup, ConfirmPopupModule} from "primeng/confirmpopup";
@@ -12,6 +12,14 @@ import {MenuModule} from "primeng/menu";
 import {ConfirmDialogModule} from "primeng/confirmdialog";
 import {InputSwitchModule} from "primeng/inputswitch";
 import {FormsModule} from "@angular/forms";
+import {BizumCreateComponent} from "../transactions/bizum-create/bizum-create.component";
+import {DialogModule} from "primeng/dialog";
+import {AvatarModule} from "primeng/avatar";
+import {CreateTransactionComponent} from "../transactions/create/create-transaction.component";
+import {PanelModule} from "primeng/panel";
+import {TableModule} from "primeng/table";
+import {TagModule} from "primeng/tag";
+import {BankaccountCreateComponent} from "../bankaccounts/bankaccount-create/bankaccount-create.component";
 
 @Component({
   selector: 'app-navbar',
@@ -26,14 +34,23 @@ import {FormsModule} from "@angular/forms";
     MenuModule,
     ConfirmDialogModule,
     InputSwitchModule,
-    FormsModule
+    FormsModule,
+    BizumCreateComponent,
+    DialogModule,
+    AvatarModule,
+    CreateTransactionComponent,
+    DatePipe,
+    PanelModule,
+    TableModule,
+    TagModule,
+    BankaccountCreateComponent
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(@Inject(DOCUMENT) private document: Document, private indexService: IndexService) {
+  constructor(@Inject(DOCUMENT) private document: Document, private indexService: IndexService, private messageService: MessageService) {
     let theme = window.localStorage.getItem('theme');
     if (theme) {
       this.themeSelection = theme == 'dark';
@@ -46,6 +63,16 @@ export class NavbarComponent implements OnInit {
   items: MenuItem[];
   visibleSidebar: boolean = false;
   themeSelection: boolean = false;
+
+  displayDialogBankAccount: boolean = false;
+  displayDialogBankAccountNewUser: boolean = false;
+  displayCreateBankAccount: boolean = false;
+  displayDialogTransaction: boolean = false;
+  displayDialogBizum: boolean = false;
+
+  @ViewChild(CreateTransactionComponent) transactionCreate!: CreateTransactionComponent
+  @ViewChild(BankaccountCreateComponent) bankAccountCreate!: BankaccountCreateComponent
+  @ViewChild(BizumCreateComponent) bizumCreateComponent!: BizumCreateComponent
 
   ngOnInit() {
     this.indexService.getUserByToken().subscribe(
@@ -103,5 +130,64 @@ export class NavbarComponent implements OnInit {
     this.document.documentElement.classList.remove('dark-theme', 'light-theme');
     this.document.documentElement.classList.add(theme + '-theme');
     themeLink.href = 'aura-' + theme + '-blue.css';
+  }
+
+  goToCreateBankAccount() {
+    this.bankAccountCreate.loadUser(this.user);
+    this.displayDialogBankAccount = true;
+  }
+
+  createBankAccount() {
+    this.displayDialogBankAccount = false;
+    this.displayDialogBankAccountNewUser = false;
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Cuenta creada',
+      detail: 'La cuenta se ha creado correctamente',
+      life: 2000,
+      closable: false
+    });
+  }
+
+  goToCreateTransaction() {
+    this.transactionCreate.loadUser();
+    this.displayDialogTransaction = true;
+  }
+
+  createTransaction() {
+    this.displayDialogTransaction = false;
+    this.transactionCreate.loadUser();
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Transacción creada',
+      detail: 'La transacción se ha creado correctamente',
+      life: 2000,
+      closable: false
+    });
+  }
+
+  goToCreateBizum() {
+    this.bizumCreateComponent.loadUser();
+    this.displayDialogBizum = true;
+  }
+
+  createBizum() {
+    this.displayDialogBizum = false;
+    this.bizumCreateComponent.loadUser()
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Bizum realizado',
+      detail: 'El Bizum se ha realizado correctamente',
+      life: 2000,
+      closable: false
+    });
+  }
+
+  closeDialog() {
+    this.displayDialogBankAccount = false;
+    this.displayDialogTransaction = false;
+    this.displayDialogBizum = false;
+    this.displayDialogBankAccountNewUser = false;
+    this.displayCreateBankAccount = false;
   }
 }
