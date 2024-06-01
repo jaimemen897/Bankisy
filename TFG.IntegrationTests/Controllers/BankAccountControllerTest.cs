4a043d828243ce1719d8afb9de3c406be7943325
jaimemen897 <jaimemen897@gmail.com>
2024-06-01 13:21:44 +0200

    Se crea primer test de integracion

--- TFG.IntegrationTests/Controllers/BankAccountControllerTest.cs
+++ TFG.IntegrationTests/Controllers/BankAccountControllerTest.cs
@@ -1,15 +1,61 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using TFG.Context.DTOs.users;
+using TFG.Services;
+
 namespace TFG.IntegrationTests.Controllers;
 
 public class BankAccountControllerTest
 {
+    private WebApplicationFactory<Program> _factory;
+    private HttpClient _client;
+    private string _token;
+
     [SetUp]
     public void Setup()
     {
+        _factory = new WebApplicationFactory<Program>();
+        _client = _factory.CreateClient();
+        var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+        using var scope = scopeFactory.CreateScope();
+        var sessionService = scope.ServiceProvider.GetService<SessionService>();
+        var response = sessionService.Login(new UserLoginDto
+        {
+            Username = "admin",
+            Password = "Admin1234"
+        }).Result;
+
+        // Parse the JSON response to get the token
+        var jsonResponse = JObject.Parse(response);
+        _token = jsonResponse["token"].ToString();
+
+        _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {_token}");
     }
 
-    [Test]
-    public void Test1()
+    [TearDown]
+    public void TearDown()
     {
-        Assert.Pass();
+        _client?.Dispose();
+        _factory?.Dispose();
+    }
+    
+    
+    [Test]
+    public async Task GetBankAccount_ReturnsExpectedResult()
+    {
+        // Arrange
+        var iban = "ES7357810375677605638221";
+
+        // Act
+        var response = await _client.GetAsync($"/BankAccounts/{iban}");
+
+        // Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        
+        var content = await response.Content.ReadAsStringAsync();
+        
+        Assert.IsNotNull(content);
     }
 }
